0d896aa9aa7e8d9f96d575e99f05cd20ea4a9f16
Jeff Schuler <jeff@websubstrate.com>
2016-05-17 10:01:02 -0600

    New back-end controller: Allow for multi-level paths.

--- application/controllers/docsnew.php
+++ application/controllers/docsnew.php
@@ -13,9 +13,18 @@ function index($which=null) {
     //if (! is_ssl() ) return $this->load->view('docsnew/sslrequired.phtml');
     if (! $this->session->userdata('admin') ) return redirect(ssl_url('administration/login'));
 
-    // prune out any non-word characters to prevent filename attacks, then load $which.phtml
-    $which = preg_replace('/\W/', '', $which);
-    if (! $which) $which = "mainmenu";
+    // Clean and assemble our path.
+    // Each '/'-separated path element in the URL string will be passed as an argument to this function.
+    $args = func_get_args();
+    $path_parts = array();
+    foreach ($args as $arg) {
+      // Prune out any non-word characters to prevent filename attacks
+      $path_parts[] = preg_replace('/\W/', '', $arg);
+    }
+    $which = implode('/', $path_parts);
+
+    // then load $which.phtml
+    if (! $which) $which = "home";
     $this->pagename = $which;
     $this->load->view("docsnew/$which.phtml");
 }
