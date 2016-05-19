72e675ccc27e71dea3858396571bc6063301eb6b
Jeff Schuler <jeff@websubstrate.com>
2016-05-18 22:05:21 -0600

    Minor code whitespace+cleanup on controllers/admin.php.

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -9,27 +9,26 @@ function __construct() {
 
 
 function index($which=null) {
-    // must be logged in and using SSL to do this
-    //if (! is_ssl() ) return $this->load->view('admin/sslrequired.phtml');
-    if (! $this->session->userdata('admin') ) return redirect(ssl_url('administration/login'));
+  // must be logged in and using SSL to do this
+  //if (! is_ssl() ) return $this->load->view('admin/sslrequired.phtml');
+  if (! $this->session->userdata('admin') ) return redirect(ssl_url('administration/login'));
 
-    $data['mainmenu'] = $this->_generate_menu_markup($this->_mainmenu_array(), $this->uri->uri_string);
+  $data['mainmenu'] = $this->_generate_menu_markup($this->_mainmenu_array(), $this->uri->uri_string);
 
-    // Clean and assemble our path.
-    // Each '/'-separated path element in the URL string will be passed as an argument to this function.
-    $args = func_get_args();
-    $path_parts = array();
-    foreach ($args as $arg) {
-      // Prune out any non-word characters to prevent filename attacks
-      $path_parts[] = preg_replace('/\W/', '', $arg);
-    }
-    $which = implode('/', $path_parts);
+  // Clean and assemble our path.
+  // Each '/'-separated path element in the URL string will be passed as an argument to this function.
+  $args = func_get_args();
+  $path_parts = array();
+  foreach ($args as $arg) {
+    // Prune out any non-word characters to prevent filename attacks
+    $path_parts[] = preg_replace('/\W/', '', $arg);
+  }
+  $which = implode('/', $path_parts);
 
-    // then load $which.phtml
-    if (! $which) $which = "home";
-    $this->pagename = $which;
-    //$this->get_main_menu() = get_main_menu();
-    $this->load->view("admin/$which.phtml", $data);
+  // then load the appropriate page (with .phtml extension)
+  if (!$which) $which = "home";
+  $this->pagename = $which;
+  $this->load->view("admin/$which.phtml", $data);
 }
 
 /*
