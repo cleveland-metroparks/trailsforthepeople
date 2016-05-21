71c29150b1d978574c88f284c80714328ba9fa59
Jeff Schuler <jeff@websubstrate.com>
2016-05-21 07:16:03 -0600

    Add JS/CSS includes from administration & contributors <head>s to default back-end template.

--- application/views/backend_base.php
+++ application/views/backend_base.php
@@ -16,14 +16,39 @@
 
     <!-- Bootstrap -->
     <link href="<?= ssl_url('static/admin/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
-    <link href="<?= ssl_url('static/admin/local.css'); ?>" rel="stylesheet">
-
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <!-- jQuery et al -->
+    <script type="text/javascript" src="<?= ssl_url('static/common/jquery-1.7.2.min.js'); ?>"></script>
+    <script type="text/javascript" src="<?= ssl_url('static/common/jquery-ui-1.8.21.custom.min.js'); ?>"></script>
+    <link type="text/css" rel="stylesheet" href="<?= ssl_url('static/common/jquery-ui-lightness/jquery-ui-1.8.20.custom.css'); ?>" />
+
+    <!-- tablesorter -->
+    <script type="text/javascript" src="<?= ssl_url('static/common/tablesorter/jquery.tablesorter.js'); ?>"></script>
+    <link type="text/css" rel="stylesheet" href="<?= ssl_url('static/common/tablesorter/themes/blue/style.css'); ?>" />
+
+    <!-- Leaflet map API and Wicket plugin for parsing WKT -->
+    <link rel="stylesheet" type="text/css" href="<?= ssl_url('static/common/leaflet-0.7.1/leaflet.css'); ?>" />
+    <script type="text/javascript" src="<?= ssl_url('static/common/leaflet-0.7.1/leaflet.js'); ?>"></script>
+    <script type="text/javascript" src="<?= ssl_url('static/common/wicket/wicket.js'); ?>"></script>
+    <script type="text/javascript" src="<?= ssl_url('static/common/wicket/wicket-leaflet.js'); ?>"></script>
+
+    <!-- TinyMCE, the HTML editor -->
+    <script type="text/javascript" src="<?= ssl_url('static/tinymce/jscripts/tiny_mce/jquery.tinymce.js'); ?>"></script>
+
+    <!-- Local back-end CSS -->
+    <link href="<?= ssl_url('static/admin/local.css'); ?>" rel="stylesheet">
+    <!-- Local common JS -->
+    <script type="text/javascript" src="<?= ssl_url('static/common/constants.js'); ?>"></script>
+    <!-- Local administration JS -->
+    <script type="text/javascript" src="<?= ssl_url('static/admin/admin.js'); ?>"></script>
+    <!-- Local contributors JS -->
+    <script type="text/javascript" src="<?= ssl_url('static/contributors/contributors.js'); ?>"></script>
   </head>
   <body class="<?= $body_classes ?>">
     <nav class="navbar navbar-inverse navbar-fixed-top">
