b19e2e7a6e9242e6bd32e2fcb13427e3bbd69b34
Jeff Schuler <jeff@websubstrate.com>
2016-05-21 08:27:42 -0600

    Make contributors controller inherit from MY_Controller.

--- application/controllers/contributors.php
+++ application/controllers/contributors.php
@@ -1,5 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Contributors extends CI_Controller {
+class Contributors extends MY_Controller {
 
 
 function __construct() {
