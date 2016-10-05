d3da3c5e6e7297a80956605b6e68c5ed4c2418ae
Jeff Schuler <jeff@websubstrate.com>
2016-10-05 11:11:56 -0600

    Add basic Grunt config/packaging to do Sass conversion (and prepare for JS minification).

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      dist: {
+        files: {
+          //'static/mobile/mobile.jscompress.js': ['static/mobile/mobile.js'],
+          //'static/desktop/desktop.jscompress.js': ['static/desktop/desktop.js'],
+          //'static/common/constants.jscompress.js': ['static/common/constants.js'],
+          //'static/desktop/common.jscompress.js': ['static/desktop/common.js']
+        }
+      }
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'compact'
+        },
+        files: {
+          'static/mobile/mobile.css': 'static/mobile/mobile.scss'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // Default tasks
+  grunt.registerTask('default', ['uglify', 'sass']);
+
+};
