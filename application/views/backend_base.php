8b43fd540fd5bc050674b0d97219a3a2ae69b610
Jeff Schuler <jeff@websubstrate.com>
2016-12-23 09:56:52 -0700

    Move tablesorter lib into static/common/libraries.

--- application/views/backend_base.php
+++ application/views/backend_base.php
@@ -33,7 +33,7 @@
     <link type="text/css" rel="stylesheet" href="<?= ssl_url('static/common/libraries/jquery-ui-1.11.4/jquery-ui.min.css'); ?>" />
 
     <!-- tablesorter -->
-    <script type="text/javascript" src="<?= ssl_url('static/common/tablesorter/jquery.tablesorter.js'); ?>"></script>
+    <script type="text/javascript" src="<?= ssl_url('static/common/libraries/tablesorter/jquery.tablesorter.js'); ?>"></script>
 
     <!-- Leaflet map API and Wicket plugin for parsing WKT -->
     <link rel="stylesheet" type="text/css" href="<?= ssl_url('static/common/libraries/leaflet-0.7.1/leaflet.css'); ?>" />
