0d26a01e0b8150f60f4e567eb0efb882c3d089c9
Jeff Schuler <jeff@websubstrate.com>
2017-01-11 15:38:14 -0700

    Add models (and table schemas) for new FDW-based Attraction & Reservation tables.

---
+++ application/models/attraction.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Attraction data model
+ */
+class Attraction extends DataMapper {
+
+var $table    = 'view_cmp_gisattractions';
+
+function __construct($id = NULL) {
+    parent::__construct($id);
+}
+
+}
+
+
+/**
+
+Schema creation
+(@TODO: move this into migration routines.)
+---------------
+
+CREATE TABLE IF NOT EXISTS view_cmp_gisattractions (
+  record_id integer,
+  src varchar(255),
+  gis_id integer,
+  pagetitle varchar(255),
+  categories varchar(255),
+  descr varchar(2000),
+  activities varchar(255),
+  reservation integer,
+  pagethumbnail varchar(255),
+  reservable boolean,
+  cmp_url varchar(255),
+  amenities varchar(255),
+  latitude varchar(255),
+  longitude varchar(255),
+  drivingdestinationlatitude varchar(255),
+  drivingdestinationlongitude varchar(255),
+  northlatitude varchar(255),
+  northlongitude varchar(255),
+  eastlatitude varchar(255),
+  eastlongitude varchar(255),
+  southlatitude varchar(255),
+  southlongitude varchar(255),
+  westlatitude varchar(255),
+  westlongitude varchar(255),
+  PRIMARY KEY(record_id)
+);
+GRANT ALL PRIVILEGES ON TABLE view_cmp_gisattractions TO trails;
+
+*/
