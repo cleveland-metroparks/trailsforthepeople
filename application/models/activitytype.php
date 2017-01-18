47c3bf889da11703d044165bb1d7bd7fe44dc546
Jeff Schuler <jeff@websubstrate.com>
2017-01-18 15:10:40 -0700

    Add more Activity icons, and expand the list of chosen activities.

--- application/models/activitytype.php
+++ application/models/activitytype.php
@@ -8,80 +8,57 @@ class ActivityType extends DataMapper {
 var $table    = 'view_cmp_gisactivitytype';
 
 var $chosen_activities = array(
-	'Swimming',
-	'Boating & Paddlesports',
-	'Hiking & Walking',
-	'Fishing & Ice Fishing',
-	'Archery',
-	'Horseback Riding',
-	'Mountain Biking',
-	'Picnicking',
-	'Sledding',
-	'Tobogganing',
-	'Exploring Nature',
-	'Exploring Culture & History',
-	'Dining',
-	//'Fitness Circuit',
-	//'Exercising',
+    'Archery',
+    'Biking & Cycling',
+    'Boating & Paddlesports',
+    'Cross-Country Skiing',
+    'Dining',
+    //'Exercising',
+    'Exploring Culture & History',
+    'Exploring Nature',
+    'Fishing & Ice Fishing',
+    //'Fitness Circuit',
+    'Geocaching',
+    'Golfing',
+    'Hiking & Walking',
+    'Horseback Riding',
+    'Mountain Biking',
+    'Picnicking',
+    'Rope Courses & Zip Lines',
+    'Sledding',
+    'Snowshoeing',
+    'Swimming',
+    'Tobogganing',
 );
-// @TODO: Move this into the DB:
-var $activity_type_icons_by_name = array(
-	'Biking & Cycling' => '',
-	'Swimming' => 'swimming.svg',
-	'Boating & Paddlesports' => 'boating.svg',
-	'Hiking & Walking' => '',
-	'Fishing & Ice Fishing' => 'fishing.svg',
-	'Archery' => '../../wms/pois/archery.png',
-	'Cross-Country Skiing' => '',
-	'Geocaching' => '',
-	'Horseback Riding' => '../../wms/pois/horse.png',
-	'Mountain Biking' => 'mtnbiking.svg',
-	'Picnicking' => 'picnic.svg',
-	'Races & Competitions' => '',
-	'Sledding' => 'sledding.svg',
-	'Snowshoeing' => '',
-	'Tobogganing' => '',
-	'Rope Courses & Zip Lines' => '',
-	'Exploring Nature' => '../../wms/pois/nature.png',
-	'Exploring Culture & History' => '../../wms/pois/history.png',
-	'Dining' => 'dining.svg',
-	'Classes, Workshops, & Lectures' => '',
-	'Special Events & Programs' => '',
-	'Concerts & Movies' => '',
-	'Fitness Circuit' => '',
-	'Disc Golf' => '',
-	'Golfing' => 'golfing.svg',
-	'Exercising' => '',
-	'FootGolf' => ''
-);
+// @TODO: Move this into the DB
 var $activity_type_icons_by_id = array(
-	01 => '',
-	02 => 'swimming.svg',
-	03 => 'boating.svg',
-	04 => '',
-	05 => 'fishing.svg',
-	06 => '../../wms/pois/archery.png',
-	07 => '',
-	09 => '',
-	11 => '../../wms/pois/horse.png',
-	12 => 'mtnbiking.svg',
-	13 => 'picnic.svg',
-	14 => '',
-	15 => 'sledding.svg',
-	16 => '',
-	17 => '',
-	18 => '',
-	19 => '../../wms/pois/nature.png',
-	20 => '../../wms/pois/history.png',
-	21 => 'dining.svg',
-	22 => '',
-	23 => '',
-	24 => '',
-	25 => '',
-	26 => '',
-	30 => 'golfing.svg',
-	39 => '',
-	41 => ''
+    01 => '',              // Biking & Cycling
+    02 => 'swim.svg',      // Swimming
+    03 => 'boat.svg',      // Boating & Paddlesports
+    04 => 'hike.svg',      // Hiking & Walking
+    05 => 'fish.svg',      // Fishing & Ice Fishing
+    06 => 'archery.svg',   // Archery
+    07 => '',              // Cross-Country Skiing
+    09 => '',              // Geocaching
+    11 => 'horse.svg',     // Horseback Riding
+    12 => 'mtnbike.svg',   // Mountain Biking
+    13 => 'picnic.svg',    // Picnicking
+    14 => '',              // Races & Competitions
+    15 => 'sled.svg',      // Sledding
+    16 => '',              // Snowshoeing
+    17 => '',              // Tobogganing
+    18 => '',              // Rope Courses & Zip Lines
+    19 => 'geology.svg',   // Exploring Nature
+    20 => 'history.svg',   // Exploring Culture & History
+    21 => 'dine.svg',      // Dining
+    22 => '',              // Classes, Workshops, & Lectures
+    23 => '',              // Special Events & Programs
+    24 => '',              // Concerts & Movies
+    25 => 'fitness.svg',   // Fitness Circuit
+    26 => '',              // Disc Golf
+    30 => 'golf.svg',      // Golfing
+    39 => 'fitness.svg',   // Exercising
+    41 => '',              // FootGolf
 );
 
 function __construct($id = NULL) {
@@ -114,27 +91,12 @@ function getActivityTypesAndIcons($chosen_only=FALSE) {
 }
 
 /**
- * Get activity icon by name
- */
-function getActivityIconByName($activity_type) {
-	if (!empty($this->activity_type_icons_by_name[$activity_type])) {
-		return $this->activity_type_icons_by_name[$activity_type];
-	} else if (!empty($this->activity_type_icons_old[$activity_type])) {
-		return $this->activity_type_icons_old[$activity_type];
-	} else {
-		// @TODO: Make a default icon
-	}
-}
-
-/**
  * Get activity icon by ID
  */
 function getActivityIconByID($id) {
-	if (!empty($this->activity_type_icons_by_id[$id])) {
-		return $this->activity_type_icons_by_id[$id];
-	} else {
-		// @TODO: Make a default icon
-	}
+    if (!empty($this->activity_type_icons_by_id[$id])) {
+        return $this->activity_type_icons_by_id[$id];
+    }
 }
 
 }
