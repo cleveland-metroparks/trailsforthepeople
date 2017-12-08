68a52831823161ada1c623b8cb8df2310c446b63
Jeff Schuler <jeff@websubstrate.com>
2017-12-08 15:17:16 -0700

    Add icons for amenities. Basketball broken.

--- application/models/amenitytype.php
+++ application/models/amenitytype.php
@@ -14,6 +14,10 @@ function __construct($id = NULL) {
 
 /**
  * Get list of amenities to be used in Amenities listing from Welcome pane.
+ *
+ * @return array
+ *   Array of amenitites, each in the form:
+ *   id => ['title' => title, 'icon' => icon]
  */
 function get_welcome_pane_amenities() {
     $amenities = new AmenityType();
@@ -22,9 +26,39 @@ function get_welcome_pane_amenities() {
         FROM $amenities->table_welcome_pane_choices;
     ";
     $results = $amenities->db->query($sql)->result();
-    $output = (array)$results;
+
+    $output = array();
+    foreach($results as $amenity) {
+        $record = array(
+            'title' => $amenity->name,
+            'icon' => AmenityType::get_icon_name_for_amenity($amenity->amenitytypeid),
+        );
+        $output[$amenity->amenitytypeid] = $record;
+    }
 
     return $output;
+}
+
+/**
+ * Get the icon name (from our icon font) for an amenity.
+ *
+ * @param int $amenity_id
+ *
+ * @return string
+ */
+function get_icon_name_for_amenity($amenity_id) {
+    // @TODO: Move this into the DB
+    $amenity_icons = array(
+        221 => 'baseball',     // Ball Field
+        231 => 'basketball',   // Basketball Court
+        280 => 'boat_rental',  // Boat Rentals
+        14  => 'drink',        // Drinking Fountain
+        243 => 'playground',   // Play Area
+        13  => 'restroom',     // Restrooms
+        28  => 'gifts',        // Shopping/Souvenirs
+        240 => 'volleyball',   // Volleyball Courts
+    );
+    return $amenity_icons[$amenity_id];
 }
 
 }
