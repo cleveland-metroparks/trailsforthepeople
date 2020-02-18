435151a4bd110a333d3193781c5b2777aec7648f
Jeff Schuler <jeff@websubstrate.com>
2020-02-18 13:01:52 -0700

    Make wicket lib object globally available.

--- static/src/js/deps-app.js
+++ static/src/js/deps-app.js
@@ -3,7 +3,7 @@ window.FastClick = require('fastclick');
 // require('jquery-mobile');
 window.mapboxgl = require('mapbox-gl');
 // require('sidebar-v2/js/jquery-sidebar');
-require('wicket');
+window.Wkt = require('wicket');
 window.turf = require('@turf/turf');
 require('@popperjs/core');
 window.Tooltip = require('tooltip.js');
