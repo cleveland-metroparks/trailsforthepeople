49f6c57f9287d0c83a1b84d3f368eff20a042d9e
Jeff Schuler <jeff@websubstrate.com>
2020-04-02 13:48:16 -0400

    Add our DB update fixes for reservation bboxes to the materialized view sync script.

--- static/import/db_update_matviews.sql
+++ static/import/db_update_matviews.sql
@@ -47,8 +47,8 @@ UPDATE view_cmp_gisattractions v
 SET activities = t.activities
 FROM temp_activity_updates t
 WHERE (v.gis_id = t.gis_id)
-	AND (v.gis_id IS NOT NULL)
-	AND (v.pagetitle = t.pagetitle);
+        AND (v.gis_id IS NOT NULL)
+        AND (v.pagetitle = t.pagetitle);
 END;
 
 
@@ -74,6 +74,20 @@ INSERT INTO view_cmp_gisreservations
 -- Recreate geometries:
 UPDATE view_cmp_gisreservations SET geom = ST_Transform(ST_SetSRID(ST_MakePoint(longitude, latitude), 4326),3734);
 UPDATE view_cmp_gisreservations SET drivingdestination_geom = ST_Transform(ST_SetSRID(ST_MakePoint(drivingdestinationlongitude, drivingdestinationlatitude), 4326),3734);
+--- Re-add bboxes from our manually-managed reservation_bbox table:
+WITH bboxes AS (
+    SELECT *
+    FROM reservation_bbox
+)
+UPDATE view_cmp_gisreservations
+SET
+    boxw = b.boxw,
+    boxs = b.boxs,
+    boxe = b.boxe,
+    boxn = b.boxn
+FROM bboxes b
+WHERE b.reservation_id = record_id;
+---
 END;
 
 
