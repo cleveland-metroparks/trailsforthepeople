383f0a73b9fd0b87806be20dba379cf67e9ade27
Jeff Schuler <jeff@websubstrate.com>
2022-06-14 17:18:31 +0200

    backend: Add initial Mantine AppShell.

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,25 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  AppShell,
+  Navbar,
+  Header,
+  Footer,
+  Aside,
+  Text,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+} from '@mantine/core';
+
+import { NavLinks } from './_navLinks';
+
 function App() {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <AppShell
+      padding="md"
+
+      navbar={
+        <Navbar p="md" hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 300 }}>
+          <Text>Application navigation</Text>
+        </Navbar>
+      }
+
+      aside={
+        <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
+          <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
+            <Text>Application right sidebar</Text>
+          </Aside>
+        </MediaQuery>
+      }
+
+      header={
+        <Header height={70} p="md">
+          <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+            <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size="sm"
+                color={theme.colors.gray[6]}
+                mr="xl"
+              />
+            </MediaQuery>
+
+            <Text>Maps Admin Back-end</Text>
+          </div>
+        </Header>
+      }
+
+      styles={(theme) => ({
+        main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+      })}
+
+      footer={
+        <Footer height={60} p="md">
+          This is the footer
+        </Footer>
+      }
+
+    >
+      <Text>Main content goes here.</Text>
+    </AppShell>
+
   );
 }
 
