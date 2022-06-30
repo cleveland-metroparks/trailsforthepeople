264a583c6a8297951752b9c1c343b6aacfb791c7
Jeff Schuler <jeff@websubstrate.com>
2022-06-30 15:00:54 +0200

    backend: Add getting of individual log entry.

--- admin/src/routes/logs.tsx
+++ admin/src/routes/logs.tsx
@@ -24,11 +24,35 @@ const getAllAuditLogs = async () => {
 }
 
 //
+const getAuditLog = async (id: string) => {
+  const response = await apiClient.get<any>("/audit_logs/" + id);
+  return response.data.data;
+}
+
+//
 export function AuditLog() {
   let params = useParams();
+  let logId = params.logId ? params.logId.toString() : '';
+  const { isLoading, isSuccess, isError, data, error, refetch } = useQuery<AuditLog, Error>(['audit_log', params.logId], () => getAuditLog(logId));
   return (
     <div>
-      <h2>Log {params.logId}</h2>
+      <Anchor component={Link} to={`/logs`}>Logs</Anchor>
+      {isLoading && <div>Loading...</div>}
+      {isError && (
+        <div>{`There is a problem fetching the post data - ${error.message}`}</div>
+      )}
+        {data &&
+          <div>
+            <h2>{data.timestamp}</h2>
+            <dl>
+              <dt><strong>id:</strong></dt><dd>{data.id}</dd>
+              <dt><strong>timestamp:</strong></dt><dd>{data.timestamp}</dd>
+              <dt><strong>ipaddress:</strong></dt><dd>{data.ipaddress}</dd>
+              <dt><strong>username:</strong></dt><dd>{data.username}</dd>
+              <dt><strong>message:</strong></dt><dd>{data.message}</dd>
+            </dl>
+          </div>
+        }
     </div>
   );
 }
