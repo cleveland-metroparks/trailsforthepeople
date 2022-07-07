435d221b5b6ed276f1c6aea60265579333ec35e7
Jeff Schuler <jeff@websubstrate.com>
2022-07-07 11:57:23 +0200

    backend: Start turning individual loop screen into form.

--- admin/src/routes/loops.tsx
+++ admin/src/routes/loops.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { useQuery } from "react-query";
 import { Link, Outlet, useParams } from "react-router-dom";
-import { Table, Anchor } from '@mantine/core';
+import { Table, Anchor, TextInput, Checkbox, Button, Group, Box } from '@mantine/core';
+import { useForm } from '@mantine/form';
 
 type Loop = {
   id: number,
@@ -52,46 +53,111 @@ const getLoop = async (id: string) => {
 export function Loop() {
   let params = useParams();
   let loopId = params.loopId ? params.loopId.toString() : '';
+
   const { isLoading, isSuccess, isError, data, error, refetch } = useQuery<Loop, Error>(['loop', params.loopId], () => getLoop(loopId));
+
+  const form = useForm({
+    initialValues: {
+      test: 'This is a test',
+      // Setting these directly in the TSX below to get the API-loaded data.
+      // @TODO: Should be better to use this, so we can use form.getInputProps() below to also get other data
+    },
+    validate: {
+    },
+  });
+
   return (
     <div>
       <Anchor component={Link} to={`/loops`}>Loops</Anchor>
+
       {isLoading && <div>Loading...</div>}
       {isError && (
-        <div>{`There is a problem fetching the post data - ${error.message}`}</div>
+        <div>{`There is a problem fetching the loop - ${error.message}`}</div>
       )}
         {data &&
           <div>
             <h2>{data.name}</h2>
-            <dl>
-              <dt><strong>id:</strong></dt><dd>{data.id}</dd>
-              <dt><strong>name:</strong></dt><dd>{data.name}</dd>
-              <dt><strong>res:</strong></dt><dd>{data.res}</dd>
-              <dt><strong>bike:</strong></dt><dd>{data.bike}</dd>
-              <dt><strong>hike:</strong></dt><dd>{data.hike}</dd>
-              <dt><strong>bridle:</strong></dt><dd>{data.bridle}</dd>
-              <dt><strong>mountainbike:</strong></dt><dd>{data.mountainbike}</dd>
-              <dt><strong>description:</strong></dt><dd>{data.description}</dd>
-              <dt><strong>distance_feet:</strong></dt><dd>{data.distance_feet}</dd>
-              <dt><strong>distancetext:</strong></dt><dd>{data.distancetext}</dd>
-              <dt><strong>durationtext_hike:</strong></dt><dd>{data.durationtext_hike}</dd>
-              <dt><strong>durationtext_bike:</strong></dt><dd>{data.durationtext_bike}</dd>
-              <dt><strong>durationtext_bridle:</strong></dt><dd>{data.durationtext_bridle}</dd>
-              <dt><strong>lat:</strong></dt><dd>{data.lat}</dd>
-              <dt><strong>lng:</strong></dt><dd>{data.lng}</dd>
-              <dt><strong>boxw:</strong></dt><dd>{data.boxw}</dd>
-              <dt><strong>boxs:</strong></dt><dd>{data.boxs}</dd>
-              <dt><strong>boxe:</strong></dt><dd>{data.boxe}</dd>
-              <dt><strong>boxn:</strong></dt><dd>{data.boxn}</dd>
-              <dt><strong>dest_id:</strong></dt><dd>{data.dest_id}</dd>
-              <dt><strong>dd_lat:</strong></dt><dd>{data.dd_lat}</dd>
-              <dt><strong>dd_lng:</strong></dt><dd>{data.dd_lng}</dd>
-            </dl>
+            <Box sx={{ maxWidth: 800 }} mx="auto">
+
+              <form onSubmit={form.onSubmit((values) => console.log(values))}>
+
+                <p>#{data.id}</p>
+                <p>{data.res}</p>
+
+                <TextInput
+                  mt="md"
+                  required
+                  label="Name"
+                  placeholder="Loop name"
+                  value={data.name}
+                />
+
+                <TextInput
+                  mt="md"
+                  required
+                  label="Test"
+                  placeholder="Test text"
+                  {...form.getInputProps('test')}
+                />
+
+                <Checkbox
+                  mt="md"
+                  label="Hiking"
+                  checked={data.hike == "Yes" ? true : false}
+                />
+
+                <Checkbox
+                  mt="md"
+                  label="Biking"
+                  checked={data.bike == "Yes" ? true : false}
+                />
+
+                <Checkbox
+                  mt="md"
+                  label="Mountain biking"
+                  checked={data.mountainbike == "Yes" ? true : false}
+                />
+
+                <Checkbox
+                  mt="md"
+                  label="Horseback"
+                  checked={data.bridle == "Yes" ? true : false}
+                />
+
+                <Group position="right" mt="md">
+                  <Button type="submit">Submit</Button>
+                </Group>
+
+              </form>
+
+              <div>
+                <h3>Stats</h3>
+                <span><strong>Distance:</strong></span> <span>{data.distancetext} ({data.distance_feet} ft)</span><br />
+                <span><strong>Hiking:</strong></span> <span>{data.durationtext_hike}</span><br />
+                <span><strong>Bicycling:</strong></span> <span>{data.durationtext_bike}</span><br />
+                <span><strong>Horseback:</strong></span> <span>{data.durationtext_bridle}</span><br />
+              </div>
+
+
+            </Box>
           </div>
         }
     </div>
   );
 }
+
+// <dl>
+//   <dt><strong>Description:</strong></dt><dd>{data.description}</dd>
+//   <dt><strong>lat:</strong></dt><dd>{data.lat}</dd>
+//   <dt><strong>lng:</strong></dt><dd>{data.lng}</dd>
+//   <dt><strong>boxw:</strong></dt><dd>{data.boxw}</dd>
+//   <dt><strong>boxs:</strong></dt><dd>{data.boxs}</dd>
+//   <dt><strong>boxe:</strong></dt><dd>{data.boxe}</dd>
+//   <dt><strong>boxn:</strong></dt><dd>{data.boxn}</dd>
+//   <dt><strong>dest_id:</strong></dt><dd>{data.dest_id}</dd>
+//   <dt><strong>dd_lat:</strong></dt><dd>{data.dd_lat}</dd>
+//   <dt><strong>dd_lng:</strong></dt><dd>{data.dd_lng}</dd>
+// </dl>
 
 //
 export function LoopsList() {
