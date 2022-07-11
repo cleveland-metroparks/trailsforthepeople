1e078146d34092923e7fb10b44bb17aec8c3f82b
Jeff Schuler <jeff@websubstrate.com>
2022-07-11 11:15:13 +0200

    backend: Get loop geojson loading on map.

--- admin/src/components/loopmap.tsx
+++ admin/src/components/loopmap.tsx
@@ -1,8 +1,7 @@
-import { useState } from 'react';
 import axios from "axios";
 import { useQuery } from "react-query";
 import { Link, Outlet, useParams } from "react-router-dom";
-import {Map, Marker} from 'react-map-gl';
+import {Map, Source, Layer, LineLayer} from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 const MAPBOX_TOKEN = 'pk.eyJ1IjoiY2xldmVsYW5kLW1ldHJvcGFya3MiLCJhIjoiY2w1Y2h5NWN4MGhxejNjbDFzOWczNXJmdyJ9.TrbioVMC_vB2cl34g6Ja8A';
@@ -42,19 +41,47 @@ export function LoopMap(props: LoopMapProps) {
 
   const { isLoading, isSuccess, isError, data, error, refetch } = useQuery<LoopGeometry, Error>(['loop_geometry', loopId], () => getLoopGeometry(loopId));
 
-  const [activeTab, setActiveTab] = useState(0);
+  const loopLayer: LineLayer = {
+    id: "highlightLine",
+    type: "line",
+    source: {
+      type: "geojson"
+    },
+    layout: {
+      "line-join": "round",
+      "line-cap": "round"
+    },
+    paint: {
+      "line-color": "#01B3FD",
+      "line-width": 6,
+      "line-opacity": 0.75
+    }
+  };
 
   return (
-    <Map
-      initialViewState={{
-        latitude: MAP_DEFAULT_STATE.latitude,
-        longitude: MAP_DEFAULT_STATE.longitude,
-        zoom: MAP_DEFAULT_STATE.zoom
-      }}
-      style={{width: 600, height: 400}}
-      mapStyle={MAPBOX_STYLE}
-      mapboxAccessToken={MAPBOX_TOKEN}
-    >
-    </Map>
+    <div>
+      {isLoading && <div>Loading...</div>}
+
+      {isError && (
+        <div>{`There is a problem fetching the loop - ${error.message}`}</div>
+      )}
+
+      {data &&
+        <Map
+          initialViewState={{
+            latitude: MAP_DEFAULT_STATE.latitude,
+            longitude: MAP_DEFAULT_STATE.longitude,
+            zoom: MAP_DEFAULT_STATE.zoom
+          }}
+          style={{width: 600, height: 400}}
+          mapStyle={MAPBOX_STYLE}
+          mapboxAccessToken={MAPBOX_TOKEN}
+        >
+          <Source type="geojson" data={JSON.parse(data.geom_geojson)}>
+            <Layer {...loopLayer} />
+          </Source>
+        </Map>
+      }
+    </div>
   );
 }
