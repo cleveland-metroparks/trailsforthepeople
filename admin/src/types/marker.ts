d41bcb95a280bdf0cc1b14f0631a30f78c70d19d
Jeff Schuler <jeff@websubstrate.com>
2023-01-31 11:20:08 +0100

    Backend: Get new marker save working.

--- admin/src/types/marker.ts
+++ admin/src/types/marker.ts
@@ -15,3 +15,27 @@ export type Marker = {
     startdate: string,
     modified: string
 };
+
+const markerCategories = [
+    'Trails',
+    'Trail Closures and Construction',
+];
+
+export const defaultMarkerCategory = 'Trails';
+
+export const markerCategorySelectOptions = markerCategories.map(cat => ({
+    value: cat,
+    label: cat,
+}));
+
+export type MarkerFormData = {
+    title: string,
+    content: string,
+    category: string,
+    enabled: boolean,
+    annual: boolean,
+    startDate,
+    expireDate,
+    latitude: number,
+    longitude: number
+};
