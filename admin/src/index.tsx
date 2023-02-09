36b0957860a9d848e42e64522356b5e45e9666e4
Jeff Schuler <jeff@websubstrate.com>
2023-02-09 08:36:25 +0100

    Backend: Home tweaks.

--- admin/src/index.tsx
+++ admin/src/index.tsx
@@ -10,7 +10,7 @@ import {
   createBrowserRouter,
 } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import { Home } from "./routes/home";
 import { LoopsList, LoopEdit } from "./routes/loops";
--- admin/src/routes/home.tsx
+++ admin/src/routes/home.tsx
@@ -5,19 +5,23 @@ import { NavButtons } from '../_navLinks';
 export function Home() {
   return (
     <>
-      <Title sx={{margin: '150px 0 0'}} align="center" order={1}>
+      <Title sx={{margin: '4em 0 0'}} align="center" order={1}>
         Maps Content Admin
       </Title>
 
-      <Text ta="center" fz={{base: 'lg', sm: 'xl'}} sx={{margin: '1em 0'}}>
-        For the Cleveland Metroparks maps & trails <Anchor href="http://maps.clevelandmetroparks.com/">web app</Anchor> & <Anchor href="https://maps-api.clevelandmetroparks.com/api/docs#/">API</Anchor>.
+      <Text
+        ta="center"
+        fz={{base: 'lg', sm: 'xl'}}
+        sx={{margin: '2em 0 3em'}}
+      >
+        For the Cleveland Metroparks <strong>maps</strong> and <strong>trails</strong> <Anchor href="http://maps.clevelandmetroparks.com/">web app</Anchor> & <Anchor href="https://maps-api.clevelandmetroparks.com/api/docs#/">API</Anchor>.
       </Text>
 
       <Flex
         direction={{ base: 'column', md: 'row' }}
         gap={{ base: 'sm', sm: 'lg' }}
         justify={{ sm: 'center' }}
-        sx={{margin: '2em'}}
+        sx={{margin: '3em'}}
         >
         <NavButtons />
       </Flex>
