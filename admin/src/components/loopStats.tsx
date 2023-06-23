47097a426fc2804cb549397f4b66adc42cebdc67
Jeff Schuler <jeff@websubstrate.com>
2023-06-23 15:09:26 -0400

    Backend: One more distance_text => distancetext.

--- admin/src/components/loopStats.tsx
+++ admin/src/components/loopStats.tsx
@@ -7,7 +7,7 @@ export function LoopStats(props) {
   return (
     <>
       <Box>
-        <span><strong>Distance:</strong></span> <span>{props.stats.distance_text} ({props.stats.distance_feet} ft)</span><br />
+        <span><strong>Distance:</strong></span> <span>{props.stats.distancetext} ({props.stats.distance_feet} ft)</span><br />
         <span><strong>Hiking:</strong></span> <span>{props.stats.durationtext_hike}</span><br />
         <span><strong>Bicycling:</strong></span> <span>{props.stats.durationtext_bike}</span><br />
         <span><strong>Horseback:</strong></span> <span>{props.stats.durationtext_bridle}</span><br />
