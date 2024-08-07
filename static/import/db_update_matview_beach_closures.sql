90193875b8dcfd4b6ac84f3847ed135b5f43c966
Jeff Schuler <jeff@websubstrate.com>
2024-08-07 13:48:55 -0600

    Remove matview update cron scripts (moved to API side)

--- static/import/db_update_matview_beach_closures.sql
+++
@@ -1,9 +0,0 @@
---
--- Update CM Maps "materialized view" view_cmp_gisbeachclosures
---
-
-BEGIN;
-DELETE FROM view_cmp_gisbeachclosures;
-INSERT INTO view_cmp_gisbeachclosures
-  SELECT * FROM fdw_cmp_gisbeachclosures;
-END;
