e71e23049b6bd5c796f9019a0c6c08d84b01b29c
Jeff Schuler <jeff@websubstrate.com>
2025-11-20 09:37:12 +0100

    Remove web-vitals lib

--- admin/src/index.tsx
+++ admin/src/index.tsx
@@ -38,7 +38,6 @@ import { AuditLogsList, AuditLogView } from "./routes/logs";
 import './index.css';
 
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 let REACT_APP_ROOT_PATH = process.env.REACT_APP_ROOT_PATH;
 
@@ -144,8 +143,3 @@ root.render(
     </QueryClientProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
