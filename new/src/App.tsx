ce901bb4f6d7d1139d49685695481bf5b1e5ba71
Jeff Schuler <jeff@websubstrate.com>
2026-01-12 13:36:24 -0700

    New FE: Lazy-load Mapbox MapView

--- new/src/App.tsx
+++ new/src/App.tsx
@@ -1,15 +1,31 @@
 import { Routes, Route } from 'react-router-dom'
-import { MapView } from './components/MapView'
+import { lazy, Suspense } from 'react'
 import { Layout } from './components/Layout'
 import './App.css'
+
+// Lazy load MapView to defer mapbox-gl loading until needed
+const MapView = lazy(() => import('./components/MapView').then(m => ({ default: m.MapView })))
 
 function App() {
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<MapView />} />
-        {/* Add more routes as needed */}
-      </Routes>
+      <Suspense fallback={
+        <div style={{ 
+          display: 'flex', 
+          alignItems: 'center', 
+          justifyContent: 'center', 
+          height: '100%',
+          fontSize: '18px',
+          color: '#666'
+        }}>
+          Loading map...
+        </div>
+      }>
+        <Routes>
+          <Route path="/" element={<MapView />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </Suspense>
     </Layout>
   )
 }
