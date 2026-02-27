705ce3a1856f3e1224c88afa92229ff51f9f4208
Jeff Schuler <jeff@websubstrate.com>
2026-02-26 21:18:38 -0700

    New FE: Fix .eslintrc.cjs

--- new/.eslintrc.cjs
+++ new/.eslintrc.cjs
@@ -3,16 +3,13 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['@typescript-eslint', 'react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': 'warn',
   },
 }
